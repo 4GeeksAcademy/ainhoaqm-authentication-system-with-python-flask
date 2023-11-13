143fda97ffe176224c2966391171c1fa400553fa
Ainhoa <113107823+AinhoaQM@users.noreply.github.com>
2023-11-13 19:37:12 +0000

    logica terminada

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-    const access_token = localStorage.getItem("access_token");
 
     return (
         <nav className="navbar navbar-light bg-light">
