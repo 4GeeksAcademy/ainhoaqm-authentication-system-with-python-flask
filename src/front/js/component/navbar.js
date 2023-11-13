099e4936bf86a6aeffe4ae22b9b3eca3cfdcc8c1
Ainhoa <113107823+AinhoaQM@users.noreply.github.com>
2023-11-13 19:28:08 +0000

    logica terminada

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,18 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+    const access_token = localStorage.getItem("access_token");
+
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <div className="container">
+                <Link to="/" className="navbar-brand">Home</Link>
+                <Link to="/signup" className="nav-link">Signup</Link>
+                <Link to="/login" className="nav-link">Login</Link>
+            </div>
+        </nav>
+    );
 };
